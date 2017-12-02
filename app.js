e58442c6740f40ed8ee217a7185b31d52985a389
LearnAndExplore <pangpang_1993_0506@yahoo.com.hk>
2017-12-02 15:39:50 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var SiteController = require('./controllers/SiteController')
 app.get('/',SiteController.Index()
     // function(req,resp){
     //     resp.render('index')
-})
+)
 app.get('/about',function(req,resp){
   resp.render('about')
 })
