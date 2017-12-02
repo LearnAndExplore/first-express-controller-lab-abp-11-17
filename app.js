eeeb79c715f87cc3e860f3841eb37828a41373c8
LearnAndExplore <pangpang_1993_0506@yahoo.com.hk>
2017-12-02 14:54:04 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var express = require('express')
 var app = express()
 
-
+app.get('/',function(req,resp){
+  resp.render('site/index')
+})
+app.get('/about',function(req,resp){
+  resp.render('site/about')
+})
+app.get('/contact',function(req,resp){
+  resp.render('site/contact')
+})
 
 module.exports = app
 
